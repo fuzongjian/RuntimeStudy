269bc0a3269a2a16621d64d011100349de1c0a39
fuzongjian <2917066078@qq.com>
2016-04-12 09:43:27 +0800

    增加模型与字典互转类

---
+++ RuntimeStudy/RuntimeStudy/Model-Dictionary/Dictionary-Model.h
@@ -0,0 +1,13 @@
+//
+//  Dictionary-Model.h
+//  RuntimeStudy
+//
+//  Created by 陈舒澳 on 16/4/12.
+//  Copyright © 2016年 xingbida. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dictionary_Model : NSObject
+
+@end
---
+++ RuntimeStudy/RuntimeStudy/Model-Dictionary/Dictionary-Model.m
@@ -0,0 +1,13 @@
+//
+//  Dictionary-Model.m
+//  RuntimeStudy
+//
+//  Created by 陈舒澳 on 16/4/12.
+//  Copyright © 2016年 xingbida. All rights reserved.
+//
+
+#import "Dictionary-Model.h"
+
+@implementation Dictionary_Model
+
+@end
